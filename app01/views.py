45421a870f105476c7096a71375e0ea089250bc2
“@ruiyi0118” <906063940@qq.com>
2023-05-02 14:17:35 +0800

    部门展示、添加、删除、修改

---
+++ app01/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from app01 import models
+
+# Create your views here.
+def depart_list(request):
+    """ 部门列表 """
+    # 去数据库中获取部门消息
+    data_list = models.Department.objects.all()
+
+    return render(request, 'depart_list.html', {'data_list': data_list})
+
+def depart_add(request):
+    """ 添加部门 """
+    if request.method == "GET":
+        return render(request, 'depart_add.html')
+    # 获取用户提交过来的数据
+    title = request.POST.get("title")
+    # 保存到数据库
+    models.Department.objects.create(title=title)
+    return redirect("/depart/list/")
+
+def depart_delete(request):
+    """ 删除部门 """
+    nid = request.GET.get("nid")
+    models.Department.objects.filter(id=nid).delete()
+    return redirect("/depart/list/")
+
+def depart_edit(request, nid):
+    """ 修改编辑部门 """
+    if request.method == "GET":
+        obj = models.Department.objects.filter(id=nid).first()
+        return render(request, "depart_edit.html", {'obj': obj})
+
+    title = request.POST.get("title")
+    models.Department.objects.filter(id=nid).update(title=title)
+    return redirect("/depart/list/")
