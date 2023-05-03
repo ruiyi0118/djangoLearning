9dea4d5fdc01c24531f71c50764127583576a538
“@ruiyi0118” <906063940@qq.com>
2023-05-03 15:38:29 +0800

    新建靓号

--- app01/views.py
+++ app01/views.py
@@ -145,7 +145,7 @@ class PrettyModelForm(forms.ModelForm):
     # 通过正则表达式校验
     mobile = forms.CharField(
         label='手机号',
-        validators=[RegexValidator(r'^1[3,9]\d{9}$', '手机号格式错误')],
+        validators=[RegexValidator(r'^1[3-9]\d{9}$', '手机号格式错误')],
     )
     class Meta:
         model = models.PrettyNum
